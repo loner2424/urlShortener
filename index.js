2f59575834c3c1c190faf5a093101b9751706cce
loner2424 <92987477+loner2424@users.noreply.github.com>
2023-06-22 05:10:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const urlRoute = require("./routes/url");
 const URL = require("./models/url");
 const base62 = require("base62")
 require("dotenv").config()
-
+const cors = require("cors")
 const app = express();
 const PORT = 8001;
 
 connectToMongoDB(process.env.MONGO_URI).then(() =>
   console.log("Mongodb connected")
 );
-
+app.use(cors());
 app.use(express.json());
 
 app.use("/url", urlRoute);
